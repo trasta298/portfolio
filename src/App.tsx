513088cf7d0b7ccbbafef014358140f078861854
trasta <trasta298@gmail.com>
2021-02-19 01:18:50 +0900

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react'
-import { Home } from './pages/Home'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import { Home } from './pages/Home'
+import { Header } from './pages/Header'
+import { Color } from './style/Color'
 
 export const App : React.FC = () => {
   return (
-    <Router>
-      <Route exact path='/' component={Home}></Route>
-    </Router>
+    <Page>
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <Content>
+        <Router>
+          <Route exact path='/' component={Home}></Route>
+        </Router>
+      </Content>
+    </Page>
   )
 }
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+`
+
+const Content = styled.div`
+  height: calc(100vh - 100px);
+`
+
+const HeaderContainer = styled.div`
+  background-color: ${Color.main};
+  height: 100px;
+`
---
+++ src/style/Color.ts
@@ -0,0 +1,6 @@
+export const Color = {
+  textPrimary: '#280B0B',
+  textSecondaly: '#3F3009',
+  main: '#F2CF63',
+  accent: '#F2AB6D'
+}
