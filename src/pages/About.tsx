87198f51f84ac4d9ce541278beec8e4638ddb6d1
trasta <trasta298@gmail.com>
2021-03-29 21:28:30 +0900

    学年上がり

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,7 +18,7 @@ export const About = () => {
         <MyProfile>
           <h2>trasta (とらすた)</h2>
           <p>
-            東京工業大学 生命理工学院 B1<br/>
+            東京工業大学 情報通信系 B2<br/>
             <a href='https://trap.jp' target='_blank' rel='noreferrer'>
               東京工業大学デジタル創作同好会traP
             </a>
